ce95be37dd8bc8095d9269fd828ac18bf5b21555
Mingle Li <limingle5@gmail.com>
2020-02-05 20:41:34 -0500

    Added elevator, intake, winch subsystem and commands

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,14 +7,34 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.ElevatorMoveCommand;
+import frc.robot.commands.WinchSpinCommand;
 
 public class Climber extends SubsystemBase {
+
+  private final ElevatorMoveCommand c_elevator;
+  private final WinchSpinCommand c_winchSpin;
+
+  private final TalonSRX elevatorMaster;
+  private final CANSparkMax winchMaster;
+
   /**
    * Creates a new Climber.
    */
   public Climber() {
+    c_elevator = new ElevatorMoveCommand(this);
+    c_winchSpin = new WinchSpinCommand(this);
 
+    elevatorMaster = new TalonSRX(1000);
+    elevatorMaster.configFactoryDefault();
+
+    winchMaster = new CANSparkMax(1000, MotorType.kBrushless);
   }
 
   @Override
